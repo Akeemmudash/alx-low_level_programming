05c7d63f246a89f955a748b02ef3d322339b02e7
Akeem Mudashiru <akeemmudash@gmail.com>
2023-03-07 09:14:00 +0100

    locating characters

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,11 +16,14 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if ((*s == c) || (*s == '\0')
+		if (*s == c) 
 		{
 			return (s);
 		}
 			s++;
 	}
+	if (*s == '\0')
+	return (s);
+
 	return (NULL);
 }
